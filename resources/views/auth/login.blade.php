2a2d0f4349c1f60dea72c6413f4e5b29cbe547ee
dimasahmat <dimasahmat28@gmail.com>
2024-06-01 00:16:00 +0700

    menambahkan UI login page dan register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,30 +7,31 @@
             <div class="row no-gutters">
                 <div class="col-sm-6 align-self-center">
                     <div class="sign-in-from">
-                        <h1 class="mb-0">Sign in</h1>
+                        <h1 class="mb-0">Login</h1>
                         <p>Enter your email address and password to access admin panel.</p>
-                        <form class="mt-4">
+                        <form class="mt-4" action="{{ route('login') }}" method="POST">
+                            @csrf
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control mb-0" id="exampleInputEmail1"
+                                <input name="email" type="email" class="form-control mb-0" id="exampleInputEmail1"
                                     placeholder="Enter email">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Password</label>
                                 <a href="#" class="float-right">Forgot password?</a>
-                                <input type="password" class="form-control mb-0" id="exampleInputPassword1"
+                                <input name="password" type="password" class="form-control mb-0" id="exampleInputPassword1"
                                     placeholder="Password">
                             </div>
                             <div class="d-inline-block w-100">
                                 <div class="custom-control custom-checkbox d-inline-block mt-2 pt-1">
-                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                    <input name="remember" type="checkbox" class="custom-control-input" id="customCheck1">
                                     <label class="custom-control-label" for="customCheck1">Remember Me</label>
                                 </div>
-                                <button type="submit" class="btn btn-primary float-right">Sign in</button>
+                                <button type="submit" class="btn btn-primary float-right">Login</button>
                             </div>
                             <div class="sign-info">
                                 <span class="dark-color d-inline-block line-height-2">Don't have an account? <a
-                                        href="#">Sign up</a></span>
+                                        href="{{ route('register') }}">Register</a></span>
                                 <ul class="iq-social-media">
                                     <li><a href="#"><i class="ri-facebook-box-line"></i></a></li>
                                     <li><a href="#"><i class="ri-twitter-line"></i></a></li>
