c395cc777accf1adf696131c9a2df209c5e1047f
dimasahmat <dimasahmat28@gmail.com>
2024-06-05 06:55:15 +0700

    menambahkan CRUD user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,15 +13,11 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $Users = User::orderBy('name', 'asc')->get();
+        return response()->json([
+            'message'   => 'Berhasil menampilkan data user',
+            'data'      => $Users
+        ], 200);
     }
 
     /**
@@ -28,7 +25,49 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validasi data
+        $validated = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'min:3',
+                'max:255'
+            ],
+            'email' => [
+                'required',
+                'email',
+                'unique:users,email'
+            ],
+            'password' => [
+                'required',
+                'min:8'
+            ],
+            'password_confirmation' => [
+                'required',
+                'same:password'
+            ],
+            'avatar' => [
+                'nullable',
+                'image',
+                'mimes:jpg,jpeg,png',
+                'max:2048'
+            ]
+        ]);
+
+        // unggah avatar
+        if ($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $avatarPath = $avatar->store('avatars', 'public');
+
+            $validated['avatar'] = $avatarPath;
+        }
+
+        // menambahkan user baru
+        $user = User::create($validated);
+        return response()->json([
+            'message' => 'Berhasil menambahkan user baru',
+            'data' => $user
+        ], 201);
     }
 
     /**
@@ -36,15 +75,11 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        $Users = User::find($id);
+        return response()->json([
+            'message' => 'berhasil menampilkan detail user',
+            'data' => $Users
+        ], 200);
     }
 
     /**
@@ -52,7 +87,51 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        //validasi data
+        $validated = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'min:3',
+                'max:255'
+            ],
+            'email' => [
+                'required',
+                'email',
+                'unique:users,email'
+            ],
+            'password' => [
+                'required',
+                'min:8'
+            ],
+            'password_confirmation' => [
+                'required',
+                'same:password'
+            ],
+            'avatar' => [
+                'nullable',
+                'image',
+                'mimes:jpg,jpeg,png',
+                'max:2048' // 2 mb
+            ]
+        ]);
+
+        // unggah avatar
+        if ($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $avatarPath = $avatar->store('avatars', 'public');
+
+            $validated['avatar'] = $avatarPath;
+        }
+
+        // update data
+        $Users = User::find($id);
+        $Users->update($validated);
+
+        return response()->json([
+            'message' => 'berhasil mengubah data user',
+            'data' => $Users
+        ], 200);
     }
 
     /**
@@ -60,6 +139,12 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $Users = User::find($id);
+        $Users->delete();
+
+        return response()->json([
+            'message' => 'berhasil menghapus data user',
+            'data' => $Users
+        ], 200);
     }
 }
